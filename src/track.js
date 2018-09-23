55c5a723f28997b8dda6999ebd2f7b180282078c
Andrey Azov <andrey.azov@gmail.com>
2018-09-24 00:16:17 +0300

    Remove unnecessary comments

--- src/track.js
+++ src/track.js
@@ -220,11 +220,7 @@ class Track extends Component {
   }
 
   getTrackStyle() {
-    let { trackPosition } = this.props.pager;
-    // if (!this.state.instant) {
-    //   trackPosition = spring(trackPosition, this.props.springConfig);
-    // }
-    return { trackPosition };
+    return { trackPosition: this.props.pager.trackPosition };
   }
 
   handleOnRest = () => {
@@ -279,88 +275,3 @@ export default class TrackWithContext extends Component {
   }
 
 }
-
-
-
-
-
-
-
-
-// class TrackScroller extends Component {
-//
-//   state = {
-//     x: 0,
-//     y: 0
-//   }
-//
-//   render() {
-//     const { tag, children, ...restProps } = this.props;
-//     let style = {
-//       ...restProps.style
-//     };
-//
-//     return createElement(tag, {
-//       ...restProps,
-//       style
-//     }, this.renderViews());
-//   }
-//
-//   renderViews() {
-//     // we need Children map in order for the infinite option to work
-//     // not actually sure why this is the case
-//     return Children.map(this.props.children, child => child);
-//   }
-// }
-//
-// class Track extends Component {
-//
-//   static defaultProps = {
-//     tag: 'div',
-//     currentView: 0,
-//     viewsToShow: 1,
-//     viewsToMove: 1,
-//     infinite: false,
-//     instant: false,
-//     swipe: true,
-//     swipeThreshold: 0.5,
-//     flickTimeout: 300,
-//     onSwipeStart: noop,
-//     onSwipeMove: noop,
-//     onSwipeEnd: noop,
-//     onScroll: noop,
-//     onViewChange: noop,
-//     onRest: noop
-//   }
-//
-//   state = {
-//     instant: false
-//   }
-//
-//   currentTween = 0
-//   hydrate = false
-//
-//   render() {
-//     const { tag, onScroll, ...restProps } = this.props;
-//     return (
-//       <Spring
-//         from={{ translateX: 0 }}
-//         to={{ translateX: 200 }}
-//         onRest={this.props.onRest}
-//         >
-//         { ({ translateX }) =>
-//           createElement(TrackScroller, {
-//             style: {
-//               transform: `translateX(${translateX}px)`,
-//             },
-//             tag,
-//             onScroll,
-//             ...restProps
-//           })
-//         }
-//       </Spring>
-//     );
-//   }
-// }
-//
-// export default Track;
