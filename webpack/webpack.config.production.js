09fa5d669364bfff53a39e9f76ca454166646484
Andrey Azov <andrey.azov@gmail.com>
2019-01-05 01:06:15 +0300

    Update build scripts

---
+++ webpack/webpack.config.production.js
@@ -0,0 +1,61 @@
+const path = require('path');
+
+const externals = {
+  'react': {
+    root: 'React',
+    commonjs2: 'react',
+    commonjs: 'react',
+    amd: 'React'
+  },
+  'react-dom': {
+    root: 'ReactDOM',
+    commonjs2: 'react-dom',
+    commonjs: 'react-dom',
+    amd: 'ReactDOM'
+  },
+  'react-spring': {
+    root: 'ReactSpring',
+    commonjs2: 'react-spring',
+    commonjs: 'react-spring',
+    amd: 'ReactSpring'
+  },
+  'lodash': {
+    commonjs: 'lodash',
+    commonjs2: 'lodash2',
+    amd: 'lodash',
+    root: '_'
+  }
+};
+
+module.exports = (env) => {
+  const { mode } = env;
+
+  const config = {
+    mode,
+
+    entry: {
+      app: path.resolve(__dirname, '../src/index.js')
+    },
+
+    output: {
+      path: path.resolve(__dirname, '../dist'),
+      filename: 'react-view-pager.js',
+      library: 'ReactViewPager',
+      libraryTarget: 'umd'
+    },
+
+    module: {
+      rules: [
+        {
+          test: /.jsx?$/,
+          loader: 'babel-loader',
+          exclude: /node_modules/
+        }
+      ]
+    },
+
+    externals
+  };
+
+  return config;
+};
